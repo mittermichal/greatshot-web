615ba974cee2a4d40b597ba59de6279d944bfbe9
Michal Mitter <mittermichal@gmail.com>
2016-06-19 02:14:07 +0200

    hit counting

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ import urllib.request
 import gamestv
 import re
 import json
-from pydblite.pydblite import Base
+from pydblite.sqlite import Database, Table
 
 
 
@@ -15,20 +15,24 @@ app.config.from_pyfile('config.cfg')
 #http://flask.pocoo.org/docs/0.11/patterns/fileuploads/
 
 def count_hits(lines):
-  print(len(jfile))
   players=set()
   out=[]
-  db = Base()
-  db.create("player", "region")
-  db.create_index("player")
-  db.create_index("region")
+  db = Database(":memory:")
+  table = db.create('hits', ("player",'INTEGER'), ("region",'INTEGER'))
+  table.create_index("player")
+  table.create_index("region")
   for line in lines:
     j=json.loads(line)
     if 'szType' in j and j['szType']=='bulletevent':
-      db.insert(int(j['bAttacker']),int(j['bRegion']))
-  db.cursor.execute('')
-
-  print(db.cursor.fetchall())
+      table.insert(int(j['bAttacker']),int(j['bRegion']))
+  table.cursor.execute('SELECT player,region,count(*) FROM hits group by player,region')
+  ret = []
+  result = db.cursor.fetchall()
+  for row in result:
+    ret.append(list(row))
+  print(ret)
+  return ret
+  #[(0, 0, 14), (0, 130, 18), (0, 131, 177), (0, 132, 1), (1, 0, 5), (1, 130, 34),...
   return [{'num':2}]
 
 
@@ -36,23 +40,18 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in set(['tv_84','dm_84'])
 
-
 def upload(request):
   if 'uselast' in request.form:
     print('uselast')
   elif request.form['matchId']!='' and request.form['map']!='':
-    #print(re.findall('(\d+)',request.form['matchId'])[0])
     urllib.request.urlretrieve(gamestv.getDemosLinks(gamestv.getMatchDemosId( int(re.findall('(\d+)',request.form['matchId'])[0]) ))[int(request.form['map'])-1], 'upload/demo.tv_84')
-    #print(gamestv.getDemosLinks(gamestv.getMatchDemosId(int(request.form['matchId']))))
   else:
     if 'file' not in request.files:
-        #flash('No file part')
         return 'No file part'
     file = request.files['file']
     # if user does not select file, browser also
     # submit a empty part without filename
     if file.filename == '':
-        #flash('No selected file')
         return 'No selected file'
     if not file or not allowed_file(file.filename):
         return 'bad extension'
@@ -77,7 +76,7 @@ def cut():
 def export():	
     if request.method == 'POST':
         upload(request)
-        subprocess.call([ app.config['PARSERPATH'], 'indexer', app.config['INDEXER']])
+        #subprocess.call([ app.config['PARSERPATH'], 'indexer', app.config['INDEXER']])
         return render_template('export-out.html',out=open('download/out.json', 'r').read(),hits=count_hits(open('download/out.json', 'r').readlines()))
     return render_template('export.html')
 
