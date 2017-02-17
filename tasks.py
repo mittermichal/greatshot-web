bffcf9c035307371f8788ad198674f706e7416d0
Michal Mitter <mittermichal@gmail.com>
2017-02-17 04:14:04 +0100

    verbose progress in stdout

--- tasks.py
+++ tasks.py
@@ -21,18 +21,21 @@ def capture(start):
 
 @celery_app.task(name="render")
 def render(demoUrl,start,title):
-  #download demoUrl
+  print('download '+demoUrl+' '+title)
   current_task.update_state(state='PROGRESS', meta={'stage': 'downloading demo', 'i':0})
   urllib.request.urlretrieve( demoUrl, tasks_config.ETPATH+'etpro/demos/demo-render.dm_84')
 
+  print('capture '+title)
   current_task.update_state(state='PROGRESS',meta={'stage': 'capturing screenshots and sound', 'i':5 })
   capture(start)
 
+  print('encoding '+title)
   current_task.update_state(state='PROGRESS', meta={'stage': 'encoding video', 'i':30})
   p = subprocess.Popen(['ffmpeg', '-y', '-framerate', '60', '-i', 'etpro/screenshots/shot%04d.tga', '-i', 'etpro/wav/synctest.wav', '-c:a', 'libvorbis', '-shortest', 'render.mp4'],cwd=tasks_config.ETPATH, shell=True)
   p.communicate()
 
   #https://api.streamable.com
+  print('uploading '+title)
   current_task.update_state(state='PROGRESS', meta={'stage': 'uploading clip...', 'i':50})
   #return 'aaaa'
   r=requests.post('https://api.streamable.com/upload', auth=(tasks_config.STREAMABLE_NAME, tasks_config.STREAMABLE_PW), files={'render.mp4': open(tasks_config.ETPATH+'render.mp4', 'rb')} , data = {'title':title})
