31a2abcfbcb2e54f94f10606391800dc95a8e41a
Michal Mitter <mittermichal@gmail.com>
2019-02-16 14:32:51 +0100

    update worker run script for venv

--- worker.bat
+++ worker.bat
@@ -1,1 +1,2 @@
-python tasks.py worker --concurrency=1 -l info
+::python tasks.py worker --concurrency=1 -l info
+cmd /k "cd /d venv\Scripts & activate & cd /d ..\.. & python tasks.py worker --concurrency=1 -l info"
