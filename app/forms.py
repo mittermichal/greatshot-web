696903cd1a89796353323b047d6b7d63dab13cc6
Michal Mitter <mittermichal@gmail.com>
2019-02-23 19:05:26 +0100

    start end label

--- app/forms.py
+++ app/forms.py
@@ -11,8 +11,8 @@ class ExportMatchLinkForm(Form):
     map_number = StringField('Map number', render_kw={"placeholder": "1"})
 
 class CutForm(Form):
-    start = IntegerField('Start', render_kw={"step":1000}, default=0)
-    end = IntegerField('End', render_kw={"step":1000},default=2147483000)
+    start = IntegerField('Start Time', render_kw={"step":1000}, default=0)
+    end = IntegerField('End Time', render_kw={"step":1000},default=2147483000)
     cut_type = SelectField('Cut type', choices=[(0, 'SNAPNUMBER'), (1, 'SNAPTIME'), (2, 'SNAPCOUNT')], default=1)
     client_num = StringField('Client number<sup>*</sup>', render_kw={"placeholder": "0"})
     gtv_match_id = HiddenField('gtv_match_id')
