38de9d930240e1c5ec9e82f737ceb1bb8fa35632
Michal Mitter <mittermichal@gmail.com>
2019-05-12 11:02:57 +0200

    console logging, faster exec on demo load

--- render.bat
+++ render.bat
@@ -2,5 +2,5 @@
 set @vcd=%cd%
 cd %1
 del etpro\screenshots\*.tga
-et.exe +set fs_game etpro +demo demo-render +wait 150 +exec init-tga +timescale 1 +set nextdemo "exec preinit-wav"
+et.exe +viewlog 1 +logfile render.log +set fs_game etpro +set com_maxfps 125 +timescale 0 +demo demo-render +timescale 0 +wait 2 +timescale 1 +exec init-tga +condump init-tga.log +timescale 1 +set nextdemo "exec preinit-wav"
 
