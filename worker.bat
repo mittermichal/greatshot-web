654fa30bf3de25675384552b088d3da6b60ac183
Michal Mitter <mittermichal@gmail.com>
2020-05-08 14:01:21 +0200

    call celery module in worker run script

--- worker.bat
+++ worker.bat
@@ -1,2 +1,2 @@
 ::python tasks.py worker --concurrency=1 -l info
-cmd /k "cd /d venv\Scripts & activate & cd /d ..\.. & python tasks.py worker --concurrency=1 -l info"
+cmd /k "cd /d venv\Scripts & activate & cd /d ..\.. & celery -A tasks worker --concurrency=1 -l info"
