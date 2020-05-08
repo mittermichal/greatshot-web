da25e8b9018c238186bfa11b62797d4e81428af6
Michal Mitter <mittermichal@gmail.com>
2020-05-08 17:30:09 +0200

    variable rename

--- app/Libtech3.py
+++ app/Libtech3.py
@@ -8,12 +8,12 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-def cut(bin_path, input, out, start, end, type, pov):
+def cut(bin_path, input_file, output_file, start, end, type, pov):
     subprocess.call(
-        [bin_path, 'cut', input, out, str(start),
+        [bin_path, 'cut', input_file, output_file, str(start),
          str(end), str(type), str(pov)])
-    print(os.getcwd())
-    if os.stat(out).st_size == 0:
+    # print(os.getcwd())
+    if os.stat(output_file).st_size == 0:
         raise Exception("Cutted demo is empty, probably wrong start-end time interval")
 
 
