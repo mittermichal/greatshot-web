cd65f527c05912191f8429b85d23c0ce2c6103d6
Michal Mitter <mittermichal@gmail.com>
2020-06-06 20:34:58 +0200

    db render status and progress

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, SmallInteger
 from app.db import Base
 
 target_metadata = Base.metadata
@@ -8,7 +8,8 @@ class Render(Base):
     __tablename__ = 'renders'
     id = Column(Integer, primary_key=True)
     streamable_short = Column(String(8))
-    state = Column(String(50))
+    status_msg = Column(String(50))
+    progress = Column(SmallInteger())
     title = Column(String(255))
     gtv_match_id = Column(Integer())
     map_number = Column(Integer())
