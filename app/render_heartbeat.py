ede8f44e6990e9eceac71a05fdc336e131260940
Michal Mitter <mittermichal@gmail.com>
2020-06-07 16:47:42 +0200

    heartbeat missing infinite loop

--- app/render_heartbeat.py
+++ app/render_heartbeat.py
@@ -4,9 +4,10 @@ from threading import Thread
 
 
 def send_beat():
-    redis_broker.client.set('worker_last_beat', int(time()) * 1000)
-    print('sent heart beat')
-    sleep(60)
+    while True:
+        redis_broker.client.set('worker_last_beat', int(time()))
+        # print('sent heart beat')
+        sleep(60)
 
 
 Thread(target=send_beat).start()
