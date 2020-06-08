83f0495fedf7cfc5e682b51a1e80aa5ba39cd3fc
Michal Mitter <mittermichal@gmail.com>
2020-06-08 14:32:00 +0200

    script to download all streamable renders

---
+++ scripts/dl_streamable.py
@@ -0,0 +1,19 @@
+from app.db import db_session
+from app.models import Render
+import requests
+
+
+def dl_streamable(streamable_short, render_id='test'):
+    api = 'https://api.streamable.com/videos/'
+    r = requests.get(api + streamable_short)
+    if r.status_code == 200:
+        file_url = 'https:'+r.json()['files']['mp4']['url']
+        print('dl video...')
+        rv = requests.get(file_url)
+        open('download/renders/'+render_id+'.mp4', 'wb').write(rv.content)
+    else:
+        print('404')
+
+
+for render in db_session.query(Render).filter(Render.streamable_short != None):
+    dl_streamable(render.streamable_short, render.id)
